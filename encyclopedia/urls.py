fd96476331e78b4c9e8b323412333aecb428e8be
brian_goffman <brian.goffman@gmail.com>
2021-02-20 11:48:45 -1000

    Add search page Add edit page Add new page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,8 +6,9 @@ from . import views
 # TODO replace wiki/ with capitalized URL
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new/", views.new, name="new"),
-    path("random/", views.random, name="random"),
+    path("new", views.new, name="new"),
+    path("wiki/{entry}", views.random, name="random"),
     path("wiki/{entry}", views.entry, name="entry"),
-    path("edit", views.edit, name="edit")
+    path("edit", views.edit, name="edit"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,23 +4,50 @@ from . import util
 from django.db import models
 from django.http import HttpResponse, HttpResponseRedirect
 from random import randint
+from markdown2 import markdown
+from django import forms
 
+# create a new form to accept entry data
+class NewWikiForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content")
 
+class ExistingWikiForm(forms.Form):
+    entry = forms.CharField(label="Title")
+
+# index page, checks for new user or session
 # TODO capitalize URL with Python function
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def new(request):
+    if request.method == "POST":
+        form = NewWikiForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = forms.cleaned_data["content"]
+            request.session[util.save_entry(title, content)]
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "new.html", {
+                "form": form
+            })
+    else:
+        return render(request, "new.html", {
+            "form": NewWikiForm()
+        })
+
 
 # generate a random number in range of entries
-# return entry
 def random(request):
     entries = util.list_entries()
-    selection = randint(0, len(entries) -1)
+    selection = randint(0, len(entries) - 1)
     entry = util.get_entry(entries[selection])
+    entry_html = markdown(entry)
     return render(request, "encyclopedia/entry.html", {
-        "entry": entry
+        "entry": entry_html
     })
 
 
@@ -42,7 +69,32 @@ def entry(request, title):
 # TODO take title to select the correct entry
 # TODO make the button save to disk
 def edit(request):
-    entries = util.list_entries()
+    if request.method == "POST":
+        form = ExistingWikiForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data["entry"]
+            request.session["entry"] = [entry]
+            return render(request, "encyclopedia/edit.html"), {
+                "entry": entry,
+                "form": form
+            }
+        else:
+            return render(request, "encyclopedia/edit.html", {
+            "entry": random(request),
+            "form": form
+             })
+    else:
+        return render(request, "edit.html", {
+            "form": ExistingWikiForm()
+        })
+
     return render(request, "encyclopedia/edit.html", {
-        "entry": util.get_entry(entries[0])
+        "form": form
+    })
+
+# search function to find matching entry
+def search(request):
+    # title = request.POST["q"]
+    return render(request, "encyclopedia/entry.html", {
+        "entry": markdown(util.get_entry("Django"))
     })
